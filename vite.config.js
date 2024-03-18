d235d80f061ae814273ba3105dbe216ad1a0557e
Shubrotto Kumar Hawlader <skshubroto@gmail.com>
2024-03-18 16:11:05 +0600

    vite configure issue bug

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/assignment-007/",
+  // base: "/assignment-007/",
   plugins: [react()],
   build: {
     outDir: " build",
